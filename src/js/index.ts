a27ebef4a5c5d51c7faac581d472e810d45c593e
csyangbinbin <yangbinbin_ytu@163.com>
2022-02-18 16:58:39 +0800

    init commit

---
+++ src/js/index.ts
@@ -0,0 +1,9 @@
+import "../css/style.css"
+console.log("From TS")
+
+function add(x:number , y:number):number{
+    return x+y ; 
+}
+
+
+console.log(add(10,3));
---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode:"development", /*development,production*/
+    entry:"./src/js/index.ts" , 
+    resolve: {
+      extensions: ['.ts', '.js'],
+    },
+    output:{
+      filename:'js/index.js?[hash]',
+      path:path.resolve(__dirname , "dist") , 
+      clean:true
+    },
+    plugins:[  
+        new HtmlWebpackPlugin({
+            template:path.resolve(__dirname ,"src/index.html")
+        }) ,
+    ] , 
+    devtool: 'inline-source-map', 
+    devServer: {
+        static: './dist',
+      }, 
+    /*
+    optimization: {
+        runtimeChunk: 'single',
+        splitChunks: {
+            cacheGroups: {
+              vendor: {
+                test: /[\\/]node_modules[\\/]/,
+                name: 'vendors',
+                chunks: 'all',
+              },
+            },
+          },
+      },*/
+    module:{
+        rules:[
+        {
+            test: /\.tsx?$/,
+            use: 'ts-loader',
+            exclude: /node_modules/,
+        },
+        {
+            test:/\.css/i,
+            use:["style-loader" , "css-loader"]
+        },
+        {
+            test:/\.(png|svg|jpg|jpeg|gif)$/i , 
+            type:'asset/resource',
+            generator: { filename: 'static/images/[hash][ext][query]'  }
+        },
+        {
+            test: /\.(woff|woff2|eot|ttf|otf)$/i,
+            type: 'asset/resource',
+            generator: {filename: 'font/[hash][ext][query]' }
+        } , ]
+    }
+}
